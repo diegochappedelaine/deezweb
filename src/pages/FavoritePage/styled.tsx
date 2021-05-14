75e059689c6ba44eeb241072ba32dde288767213
Diego Chapedelaine <25375651+diegochappedelaine@users.noreply.github.com>
2021-05-14 13:06:17 +0200

    quick box-model fix

--- src/pages/FavoritePage/styled.tsx
+++ src/pages/FavoritePage/styled.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Layout = styled.div`
   width: 100vw;
-  min-height: 100vh;
+  min-height: calc(100vh - 62px);
 `;
 
 const Container = styled.section`
